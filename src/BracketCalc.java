158e3da2d08ac6f4b9f39c91a04e22c227f269dc
krh5058 <ken.r.hwang@gmail.com>
2013-09-03 01:52:22 -0400

    130903
    BracketCalc: Total rounds algorithm

--- src/BracketCalc.java
+++ src/BracketCalc.java
@@ -13,6 +13,8 @@ public class BracketCalc {
 	int secondRoundPlayersL;
 	int firstRoundPlayersL;
 	int firstRoundByL;
+	int totalRoundsW;
+	int totalRoundsL;
 	
 	public BracketCalc(int tpIn){
 	
@@ -59,8 +61,36 @@ public class BracketCalc {
 		
 		if (firstRoundPlayersL == 0) {
 			firstRoundPlayersL = firstRoundByL;
-			firstRoundByL = 0;
+			firstRoundByL = 0; // First round == second round
 		}
+		
+		int addRounds = log2(secondRoundPlayersW); // Additional rounds outside of the first winner's round
+		
+		if (addRounds==0){
+			System.out.println("Error in BracketCalc.log2()");
+		}
+		
+		totalRoundsW = 1 + addRounds;
+		
+		int x1;int x2;
+		if (isBase2(totalPlayersL)) {
+			x1 = 1;
+		} else {
+			x1 = 2;
+		}
+		
+		int x3 = 0;
+		if (secondRoundPlayersL==2) { // If there are no more matches to be played
+			x2 = 1;
+		} else if((addRounds-1) > (log2(secondRoundPlayersL)-1)) { // If there are more rounds of loser's entering, then there are loser rounds left
+			x3 = 1; // Add an additional round to play (not multiplied by 2)
+			x2 = 2;
+			--addRounds; // Take away from multiplier
+		} else { // Multiply by 2
+			x2 = 2;
+		}
+		
+		totalRoundsL = x1 + x3 + x2*(addRounds-1);
 		
 		if (BrackEm.debug){
 			System.out.println("Total (Winner's): " + totalPlayersW);
@@ -70,7 +100,40 @@ public class BracketCalc {
 			System.out.println("Total Players (Loser's): " + totalPlayersL);
 			System.out.println("Second Round (Loser's): " + secondRoundPlayersL);
 			System.out.println("First Round (Loser's): " + firstRoundPlayersL);
-			System.out.println("First Round By (Loser's): " + firstRoundByL);
+			System.out.println("First Round By (Loser's): " + firstRoundByL);		
+			System.out.println("Total Rounds (Winner's): " + totalRoundsW);
+			System.out.println("Total Rounds (Loser's): " + totalRoundsL);
 		}
 	}
+	
+	private int log2(int a){
+		int result = 0;
+		boolean valid = isBase2(a);
+		
+		if (valid){
+			result = (int) (Math.log(a)/Math.log(2));
+		}
+		
+		return result;
+	}
+	
+	private boolean isBase2(int a){
+		boolean valid = false;
+		for( int i : getIntArray() )
+			if (a == i){
+				valid = true;
+				break;
+			}
+		return valid;
+	}
+	
+	private int ceilLog2(int a){
+		int result = (int) Math.ceil(Math.log(a)/Math.log(2));
+		return result;
+	}
+	
+	int [] getIntArray() {
+		return twoN;
+	}
+	
 }
